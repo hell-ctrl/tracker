61bfa4ffe275660eeefa07f95255b2abd7f34a0a
m._neto <89698038+hell-ctrl@users.noreply.github.com>
2023-05-01 14:58:11 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,9 +5,9 @@ import fs from "fs";
 import cors from "cors";
 
 const app = express();
-const publicPath = path.join(path.dirname(new URL(import.meta.url).pathname), "../public");
+const publicPath = "../public";
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(express.static(publicPath));
 
@@ -18,12 +18,13 @@ app.get("/", (req, res) => {
 export const startServer = (url) => {
   app.post("/", (req, res) => {
     const data = JSON.stringify(req.body);
-    const filePath = path.join(path.dirname(new URL(import.meta.url).pathname), "logs/data.json");
-    fs.writeFileSync(filePath, data);
-    res.json({url: url})
+    fs.writeFileSync("logs/data.json", data);
+    res.json({ url: url });
   });
 
   app.listen(8080, () => {
-    console.log(`\n${chalk.cyan("[+]")}${chalk.green("Server started on port 8080")}`);
+    console.log(
+      `\n${chalk.cyan("[+]")}${chalk.green("Server started on port 8080")}`
+    );
   });
 };
